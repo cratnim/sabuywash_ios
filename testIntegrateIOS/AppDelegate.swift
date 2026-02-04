93b6c3ff0f00e7823588d17f520be506fd8bd405
kittikhun.k <kittikhunk@fpt.com>
2026-02-04 10:00:31 +0700

    feat: add firebase env
    add

--- testIntegrateIOS/AppDelegate.swift
+++ testIntegrateIOS/AppDelegate.swift
@@ -1,10 +1,13 @@
 import UIKit
 import Flutter
 import FlutterPluginRegistrant
+import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Initialize the shared engine
+
+        FirebaseApp.configure()
         GeneratedPluginRegistrant.register(with: FlutterEngineManager.shared.flutterEngine)
         return true
     }
